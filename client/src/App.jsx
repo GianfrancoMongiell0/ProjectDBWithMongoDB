58210d685a58e1de173919a3745c098312ac2243
Mongiellox <gianfranco.mongiello@correo.unimet.edu.ve>
2024-11-22 14:45:15 -0400

    prueba

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import TaskPage from './pages/TaskPage';
 import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
           <Route path='/' element={<h1 className='text-center text-4xl font-mono'>Home Page</h1>} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
-          <Route path='/tasks' element={<h1 className='text-center text-4xl font-mono'>Task Page</h1>} />
+          <Route path='/tasks' element={<TaskPage />} />
           <Route path='/add-tasks' element={<h1 className='text-center text-4xl font-mono'>New Task</h1>} />
           <Route path='/tasks/:id' element={<h1 className='text-center text-4xl font-mono'>Update Task</h1>} />
           <Route path='/profile' element={<h1 className='text-center text-4xl font-mono'>Profile</h1>} />
---
+++ client/src/pages/TaskPage.jsx
@@ -0,0 +1,7 @@
+export default function TaskPage() {
+    return (
+        <div className='container mx-auto'>
+            <h1 className='text-center text-4xl font-mono'>Task Page</h1>
+        </div>
+    )
+}
